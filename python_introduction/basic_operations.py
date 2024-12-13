eaeefacf107dce12a65ff50acba0d3aeecb65f3b
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2024-12-13 15:35:38 +0300

    first

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+result = number1 * number2
+print ("addition of " +number1 + "and" +number2 "is " + sum)
+print ("difference of " +number1 + "and" +number2 "is " + difference)
+print ("multiplication of " +number1 + "and" +number2 "is " + result)
