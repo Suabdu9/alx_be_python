3ad06a4944ef65bdee580d2bbe0c55997dba8dcb
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-01-04 11:54:23 +0300

    first

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,5 @@
+weather = input("What's the weather like today?(sunny/rainy/cold)")
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
