8785ab4b36673348c734f2e4fbafc001cfcccfcd
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-01-06 09:44:05 +0300

    correction

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,18 +4,19 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case 'high':
         if time_bound == "yes":
-            print(f"'{task}' is a high priority task that requires immediate attention today!")
+            reminder = (f"'{task}' is a high priority task that requires immediate attention today!")
         else:
-            print("'{task}' is a high priority task but you can do it anytime soon!")
+            reminder = (f"'{task}' is a high priority task but you can do it anytime soon!")
     case 'medium':
         if time_bound == "yes":
-            print("'{task}' is a medium priority task that requires average attention but needs to be done soon!")
+            reminder = (f"'{task}' is a medium priority task that requires average attention but needs to be done soon!")
         else:
-            print("'{task}' is a medium priority task that doesn't require immediate attention!")
+            reminder = (f"'{task}' is a medium priority task that doesn't require immediate attention!")
     case 'low':
         if time_bound == "yes":
-            print("'{task}' is a low priority task but have a time bound try to complete it when you have time!")
+            reminder = (f"'{task}' is a low priority task but have a time bound try to complete it when you have time!")
         else:
-            print(f"'{task}' is a low priority task. Consider completing it when you have free time")
+            reminder = (f"'{task}' is a low priority task. Consider completing it when you have free time")
     case _:
-        print("Input is not correct.")
+        reminder = ("Input is not correct.")
+print(reminder)
