edd35b924a186270909bd4120a48377b188ef48b
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-01-11 00:03:00 +0300

    correction

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,5 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-converted_temp = 0
 
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -15,10 +14,10 @@ def convert_to_fahrenheit(celsius):
 temperature = float(input("Enter the temperature to convert: "))
 specification = input("Is this temperature in celsius or fahrenheit? (C/F): ")
 if specification == "C":
-    convert_to_fahrenheit(temperature)
+    converted_temp = convert_to_fahrenheit(temperature)
     print(f"{temperature}°C is {converted_temp}°F")
 elif specification == "F":
-    convert_to_celsius(temperature)
+    converted_temp = convert_to_celsius(temperature)
     print(f"{temperature}°F is {converted_temp}°C")
 else:
     print("Incorrect input!")
