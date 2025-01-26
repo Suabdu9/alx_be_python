c88b3d6b70bfb249e8d8117af0095a28cc52c4ae
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-01-26 22:34:03 +0300

    third

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,7 +4,7 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        
+
     def __del__(self):
         print(f"Deleting {self.title}")
     def __str__(self):
